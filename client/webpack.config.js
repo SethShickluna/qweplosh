e37a7dc8fd9bf93c32a35bc0a1f8c3b616ef1f32
Austin Goodman <ag17za@brocku.ca>
2022-02-05 22:17:15 -0500

    initial frontend.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,21 +1,35 @@
 const path = require("path");
 const webpack = require("webpack");
 
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+	template: "./src/index.html",
+	filename: "./index.html"
+});
+
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve("dist"),
-    publicPath: "/",
-  },
-  module: {
-    rules:[
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-    ], 
-  },
+	entry: "./src/index.js",
+	mode: "development",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve("dist"),
+		publicPath: "/",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: "babel-loader"
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		],
+	},
+	devServer: {
+		open: true,
+	},
+	plugins: [htmlPlugin]
 }
